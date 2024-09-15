1d444a9f211b5db78645ab0099655142b3ef0bc4
Dovydas Podinskas <dovydaspo7@gmail.com>
2024-09-15 20:39:53 +0200

    Task 1 - library

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,22 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = [
+    -- Actions
+    "borrow",
+    "return",
+    "add_book",
+    "remove_book",
+    -- Genres
+    "fantasy",
+    "education",
+    "biography",
+    "childrens",
+    -- Book cover type
+    "hardcover",
+    "paperback",
+    -- Duration of borrow 
+    "days",
+    "weeks",
+    "months"
+    ]
